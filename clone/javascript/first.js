1b6cc73e228d8165bb475c34da0253814fe8570a
aminekhadraoui <miral.97@bk.ru>
2022-08-12 20:25:44 +0200

    clone twitter youtube portfolio

---
+++ clone/javascript/first.js
@@ -0,0 +1,26 @@
+
+/*function person(name,age){
+    return {
+        name,
+        age
+    }
+}
+let me = person("amine",22)
+let him = person("kmar",23)
+console.log(me)
+console.log(him)*/
+/*
+function person(name,age){
+    this.name = name
+    this.age = age
+}
+
+let me = new person("amine",22)
+let him = new person("kmar",23)
+console.log(me)
+console.log(him)*/
+/*
+let btn= document.querySelector("button")
+btn.onclick = ()=>{
+    console.log("hello")
+}*/
